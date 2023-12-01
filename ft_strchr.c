78005c8a979c7d8e9ccf35d0f4cf1aad651125e6
chayashi <chayashi@student.42.rio>
2023-12-01 14:20:53 -0300

    01

---
+++ ft_strchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chayashi <caarlostol@student.42.rio>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 20:41:48 by chayashi          #+#    #+#             */
+/*   Updated: 2023/11/10 14:18:52 by chayashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (unsigned char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (unsigned char)c)
+		return ((char *)s);
+	else
+		return (NULL);
+}
